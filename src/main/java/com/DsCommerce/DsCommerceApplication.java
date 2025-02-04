89dadad8f41bfe0b54b2d6b2ee5fca38fe23083a
JaymeFortes <jayme.fortes@edu.pucrs.br>
2025-02-04 19:37:01 -0300

    modified

--- src/main/java/com/DsCommerce/DsCommerceApplication.java
+++ src/main/java/com/DsCommerce/DsCommerceApplication.java
@@ -5,15 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsCommerceApplication implements CommandLineRunner {
+public class DsCommerceApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsCommerceApplication.class, args);
 	}
 
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
-	}
+
 }
