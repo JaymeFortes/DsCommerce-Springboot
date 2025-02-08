fd5bad6da9db4a315a6d2bff9dec22da74c2f0a8
JaymeFortes <jayme.fortes@edu.pucrs.br>
2025-02-08 17:57:08 -0300

    Create package Dto

--- src/main/java/com/DsCommerce/controlls/ProductController.java
+++ src/main/java/com/DsCommerce/controlls/ProductController.java
@@ -1,4 +1,25 @@
 package com.DsCommerce.controlls;
 
+import com.DsCommerce.entities.Product;
+import com.DsCommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
 public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public String test(){
+        Optional<Product> result = productRepository.findById(1L);
+        Product product = result.get();
+        return product.getName();
+    }
 }
---
+++ src/main/java/com/DsCommerce/dto/ProductDTO.java
@@ -0,0 +1,12 @@
+package com.DsCommerce.dto;
+
+import jakarta.persistence.Column;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+}
