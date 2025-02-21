bf9768fb01af266b527b3ba6dd51f5c0c89bc0a2
JaymeFortes <jayme.fortes@edu.pucrs.br>
2025-02-20 21:05:40 -0300

    add entity role, relationship ManyTomany between User and add UserRepository,UserDetailsProjection

---
+++ src/main/java/com/DsCommerce/service/UserService.java
@@ -0,0 +1,41 @@
+package com.DsCommerce.service;
+
+
+
+import java.util.List;
+
+import com.DsCommerce.entities.Role;
+import com.DsCommerce.entities.User;
+import com.DsCommerce.projections.UserDetailsProjection;
+import com.DsCommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
+
