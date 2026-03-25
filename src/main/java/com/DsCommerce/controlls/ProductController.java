0f1ebfdd1bd005197a8f37fd7d42f3f802474c25
Jayme Fortes <jayme.fortes@edu.pucrs.br>
2026-03-25 12:28:26 -0300

    feat: clean up unused imports and exclude additional packages in pom.xml

--- src/main/java/com/DsCommerce/controlls/ProductController.java
+++ src/main/java/com/DsCommerce/controlls/ProductController.java
@@ -2,8 +2,6 @@ package com.DsCommerce.controlls;
 
 import com.DsCommerce.dto.ProductDTO;
 import com.DsCommerce.dto.ProductMinDTO;
-import com.DsCommerce.entities.Product;
-import com.DsCommerce.repository.ProductRepository;
 import com.DsCommerce.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
